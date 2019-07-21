208c30b2fbd2ef6f660048d8b9f94ebd7c3370c1
hamid <hamid331994@gmail.com>
2019-07-21 15:53:30 +0430

    Updates

---
+++ precache-manifest.cf753232a9af90bf0636299b08be283f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec216fb6362bf1cea6371f7a4805e4dd",
+    "url": "/r-p/index.html"
+  },
+  {
+    "revision": "f6225fa4688ac02f54ae",
+    "url": "/r-p/static/css/main.7d6f5a1e.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/r-p/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f6225fa4688ac02f54ae",
+    "url": "/r-p/static/js/main.b3d3cded.chunk.js"
+  },
+  {
+    "revision": "03a49086bc95c2f497f2",
+    "url": "/r-p/static/js/runtime~main.ad9b72dd.js"
+  }
+]);
