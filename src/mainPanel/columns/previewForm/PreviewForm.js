645996160c43330d9413dc3d73315f116137af0b
hamid <hamid331994@gmail.com>
2019-11-01 18:40:25 +0330

    dev

--- src/mainPanel/columns/previewForm/PreviewForm.js
+++ src/mainPanel/columns/previewForm/PreviewForm.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import './previewForm.css';
 
 export class PreviewForm extends Component {
   render() {
+    const { title, description } = this.props.info;
     return (
       <>
         <h2 className='column-header'>
           Your Form
         </h2>
+        <div id='preview-form' className="column-body">
+          {
+            title !== ''
+              && <h2>{ title }</h2>
+          }
+          {
+            description !== ''
+              && <h4>{ description }</h4>
+          }
+        </div>
       </>
     )
   }
