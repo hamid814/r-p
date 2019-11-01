60b569c8be1e30b06a17f608d23d4f2b0d845bac
hamid <hamid331994@gmail.com>
2019-11-01 23:52:51 +0330

    dev

--- src/mainPanel/columns/formCode/FormCode.js
+++ src/mainPanel/columns/formCode/FormCode.js
@@ -1,7 +1,33 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 
-const FormCode = () => {
+const FormCode = ({ info: { action, method, title, description }, form }) => {
   const codeContainer = useRef(null);
+
+  useEffect(() => {
+    const form = document.createElement('form');
+
+    if(action !== '' && action) {
+      form.action = action
+    }
+
+    if(method !== '' && method && method !== 'none') {
+      form.method = method
+    }
+
+    const formTitle = document.createElement('h2');
+    formTitle.innerText = title
+    title !== '' && form.appendChild(formTitle)
+
+    const formDesc = document.createElement('h4');
+    formDesc.innerText = description
+    description !== '' && form.appendChild(formDesc)
+
+    console.log(form)
+
+    codeContainer.current.innerHTML = ''
+    codeContainer.current.appendChild(form)
+    // eslint-disable-next-line
+  }, [action, method, title, description]);
 
   return (
     <>
@@ -9,10 +35,10 @@ const FormCode = () => {
         Your Code
       </h2>
       <div id='final-code' className='column-body'>
-        <textarea>
+        <textarea value={codeContainer.current ? codeContainer.current.innerHTML : ''} readOnly>
             
         </textarea>
-        <div className='d-n'>
+        <div ref={codeContainer} className='d-n'>
 
         </div>
       </div>
