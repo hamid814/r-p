c1b916b8eeb12aa2edc4264ffff32c20918644ce
hamid <hamid331994@gmail.com>
2019-11-03 18:17:42 +0330

    dev

--- src/mainPanel/columns/previewForm/PreviewFormItem.js
+++ src/mainPanel/columns/previewForm/PreviewFormItem.js
@@ -1,24 +1,38 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const PreviewFormItem = ({item: { type, value, label, placeHolder, readOnly, required }}) => {
+  const itemDiv = useRef(null)
+  
   useEffect(() => {
-    
+    const theItem = document.createElement('input')
+    theItem.type = type
+
+    if(value !== '') {
+      theItem.setAttribute('value', value)
+    }
+
+    if(placeHolder !== '') {
+      theItem.setAttribute('placeholder', placeHolder)
+    }
+
+    if(required !== '') {
+      theItem.setAttribute('required', '')
+    }
+
+    if(readOnly !== '') {
+      theItem.setAttribute('readonly', '')
+    }
+
+    itemDiv.current.appendChild(theItem);
+
+    // eslint-disable-next-line
   }, []);
 
   return (
-    <div className='preview-form-item'>
+    <div className='preview-form-item' ref={itemDiv}>
       {
         label !== ''
           && <label>{ label } {required && <span className='required-star'>*</span>}</label>
-      }
-      {
-        readOnly
-          ? required
-            ? <input type={type} value={value} placeholder={placeHolder} readOnly required/>
-            : <input type={type} value={value} placeholder={placeHolder} readOnly/>
-          : required
-            ? <input type={type} defaultValue={value} placeholder={placeHolder} required/>
-            : <input type={type} defaultValue={value} placeholder={placeHolder}/>
       }
     </div>
     )
